16ce29b81c20115600ab19d441198bb69589836d
alysenko <scorpion1105@gmail.com>
2016-03-15 18:00:14 +0000

    Django initial

---
+++ ask/ask/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+#urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+#    url(r'^admin/', include(admin.site.urls)),
+#)
+
+urlpatterns = [
+	url(r'^$', 'qa.404.notfound', name='ask-home'),
+	url(r'^', include('qa.urls')),
+]
--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf   	/etc/gunicorn.d/test
+sudo ln -sf /home/box/web/ets/ask.gunicorn.conf	/etc/gunicorn.d/ask
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
