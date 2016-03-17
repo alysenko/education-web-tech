ebf7218dbc07cd687a02a9439c81e2cde8009e43
Andrey Lysenko <alysenko@users.noreply.github.com>
2016-03-17 16:37:19 +0000

    model forms

---
+++ ask/qa/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from models import Question, Answer
+
+class AskForm(forms.ModelForm):
+#	title = forms.CharField(max_length=255)
+#	text = forms.CharField(widget=forms.TextArea)
+	class Meta:
+		model = Question
+		fields = ['title', 'text']
+	def clean_title(self):
+		if self.cleaned_data['title'] == "":
+			raise forms.ValidationError("Title is empty", code='empty')
+	def clean_text(self):
+		if self.cleaned_data['text'] == "":
+			raise forms.ValidationError("Text is empty", code='empty')
+		
+
+class AnswerForm(forms.ModelForm):
+#	text = forms.CharField(widget=forms.TextArea)
+#	question = forms.IntegerField(widget=forms.HiddenInput)
+	class Meta:
+		model = Answer
+		fields = ['text', 'question']
+	def clean_text(self):
+		if self.cleaned_data['text'] == "":
+			raise forms.ValidationError("Text is empty", code='empty')
